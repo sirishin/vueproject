79ede3a744b629667e08a531de01e7dd6a20b7f4
sirishin <shinhee4364@gmail.com>
2024-05-05 21:24:04 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -9,8 +9,9 @@ module.exports = {
     historyApiFallback: true,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5000/",
+        target: "https://port-0-flask22-754g42aluyx17vx.sel5.cloudtype.app/",
         ws: true,
+        withCredentials: true,
         changeOrigin: true,
         pathRewrite: {
           "^/api": "/api/",
