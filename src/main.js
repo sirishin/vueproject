db7f4065467e024ee19e3f6077f043286c8250c3
sirishin <shinhee4364@gmail.com>
2024-05-05 23:02:53 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
 import router from "./router/index";
-import "./assets/css/nucleo-icons.css";
-import "./assets/css/nucleo-svg.css";
+// import "./assets/css/nucleo-icons.css";
+// import "./assets/css/nucleo-svg.css";
 import MaterialDashboard from "./material-dashboard";
 import axios from "axios";
 
