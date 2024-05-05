7db0ed4384795091dbba792f1b5946635d7e83f3
sirishin <shinhee4364@gmail.com>
2024-05-05 22:46:43 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
-import App from "./App.vue.js";
-import store from "./store.js";
-import router from "./router/index.js";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router/index";
 import "./assets/css/nucleo-icons.css";
 import "./assets/css/nucleo-svg.css";
 import MaterialDashboard from "./material-dashboard";
