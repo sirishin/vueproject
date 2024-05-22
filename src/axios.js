2a26715e02472d91f004e61e762f0a1184b349ab
sirishin <shinhee4364@gmail.com>
2024-05-22 17:31:18 +0900

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,1 +1,7 @@
+import axios from 'axios';
 
+const instance = axios.create({
+  baseURL: 'https://port-0-flask22-754g42aluyx17vx.sel5.cloudtype.app/' // 여기에 기본 URL을 설정합니다.
+});
+
+export default instance;
