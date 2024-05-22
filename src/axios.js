7e967a7d67bb2b05e25181d01a4404926101c98f
sirishin <shinhee4364@gmail.com>
2024-05-22 19:42:57 +0900

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // 환경 변수를 사용하여 기본 URL 설정
 const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_BASE_URL
+  baseURL: process.env.VUE_APP_API_BASE_URL,
+  withCredentials: true 
 });
 
 export default instance;
